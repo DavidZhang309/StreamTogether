c46f408368f201b4d86d666aa3b3e8a02f26c95c
David Zhang <david_zhang_309@hotmail.com>
2017-05-29 21:43:37 -0400

    Added sass compiling, Updated lobby styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
+var sass = require('gulp-sass');
 
 gulp.task('html', function() {
     return gulp.src('./src/client/html/**/*.html')
@@ -7,4 +8,10 @@ gulp.task('html', function() {
         .pipe(gulp.dest('./build/client/'))
 });
 
-gulp.task('default', ['html']);
+gulp.task('sass', function(){
+    return gulp.src('./src/client/sass/site.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/client'))
+})
+
+gulp.task('default', ['html', 'sass']);
