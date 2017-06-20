ccbaa8f3f742b0efc3256dcab9476a375bd8c586
DavidZhang309 <david_zhang_309@hotmail.com>
2017-06-20 15:08:54 -0400

    Fixed not using result of http response in lobby API

--- src/client/app/lobby.service.ts
+++ src/client/app/lobby.service.ts
@@ -2,7 +2,6 @@ import { RoomInfo } from './room';
 
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -12,11 +11,11 @@ export class LobbyService {
 
     public getRoomList(): Promise<RoomInfo[]> {
         return this.http.get('/api/room/list').toPromise()
-            .then((response) => { return response.json()['result'] as RoomInfo[] });
+            .then((response) => { return response.json().result as RoomInfo[] });
     }
 
     public createRoom(roomInfo: RoomInfo): Promise<RoomInfo> {
         return this.http.post('/api/room/create', roomInfo).toPromise()
-            .then((response) => { return response.json() as RoomInfo })
+            .then((response) => { return response.json().result as RoomInfo });
     }
 }
