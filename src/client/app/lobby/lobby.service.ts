148090d81ac26d87a6fdc3228174a580219463de
DavidZhang309 <david_zhang_309@hotmail.com>
2017-06-29 15:57:47 -0400

    Updated LobbyService be able to join room.

--- src/client/app/lobby/lobby.service.ts
+++ src/client/app/lobby/lobby.service.ts
@@ -1,3 +1,4 @@
+import { SocketService, IJoinRoomArgs } from '../socket.service';
 import { RoomArgs, RoomInfo } from '../room/room';
 
 import { Injectable } from '@angular/core';
@@ -6,8 +7,7 @@ import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class LobbyService {
-
-    public constructor(private http: Http) { }
+    public constructor(private http: Http, private socketSvc: SocketService) { }
 
     public getRoomList(): Promise<RoomInfo[]> {
         return this.http.get('/api/room/list').toPromise()
@@ -17,5 +17,9 @@ export class LobbyService {
     public createRoom(roomInfo: RoomArgs): Promise<RoomInfo> {
         return this.http.post('/api/room/create', roomInfo).toPromise()
             .then((response) => { return response.json().result as RoomInfo });
+    }
+    
+    public joinRoom(joinArgs: IJoinRoomArgs): Promise<any> {
+        return this.socketSvc.joinRoom(joinArgs);
     }
 }
